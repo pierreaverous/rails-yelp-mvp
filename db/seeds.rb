e4c4c29255406848e19b1068e2174a01a6f98c2d
pierre averous <pierreaverous42@gmail.com>
2021-05-22 11:13:47 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ Restaurant.create( name: 'Star Wars', address: "3 rue gabriel peri", category:"french")
+ 
